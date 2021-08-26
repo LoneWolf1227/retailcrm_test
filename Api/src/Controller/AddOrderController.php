bc7a7d385088bdde12729d53f99d8f47f6a60eea
progerWolf <programmerwolf8@gmail.com>
2021-08-26 07:53:40 +0500

    Refactored and some little changes.

--- Api/src/Controller/AddOrderController.php
+++ Api/src/Controller/AddOrderController.php
@@ -5,44 +5,27 @@ namespace App\Controller;
 use App\Services\ValidatorService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use RetailCrm\Api\Enum\ByIdentifier;
-use RetailCrm\Api\Exception\Client\HandlerException;
-use RetailCrm\Api\Exception\Client\HttpClientException;
-use RetailCrm\Api\Factory\SimpleClientFactory;
 use RetailCrm\Api\Interfaces\ApiExceptionInterface;
 use RetailCrm\Api\Interfaces\ClientExceptionInterface;
 use RetailCrm\Api\Model\Entity\CustomersCorporate\Company;
 use RetailCrm\Api\Model\Entity\Orders\Items\Offer;
 use RetailCrm\Api\Model\Entity\Orders\Items\OrderProduct;
 use RetailCrm\Api\Model\Entity\Orders\Order;
-use RetailCrm\Api\Model\Request\BySiteRequest;
 use RetailCrm\Api\Model\Request\Orders\OrdersCreateRequest;
 
 class AddOrderController extends Controller
 {
-    public function __construct(private ValidatorService $validatorService)
-    {
-    }
-
     /**
-     * @throws \RetailCrm\Api\Exception\Api\ApiErrorException
-     * @throws ClientExceptionInterface
-     * @throws HandlerException
-     * @throws \RetailCrm\Api\Exception\Api\MissingCredentialsException
-     * @throws \RetailCrm\Api\Exception\Api\AccountDoesNotExistException
-     * @throws ApiExceptionInterface
-     * @throws HttpClientException
-     * @throws \RetailCrm\Api\Exception\Api\MissingParameterException
-     * @throws \RetailCrm\Api\Exception\Client\BuilderException
-     * @throws \RetailCrm\Api\Exception\Api\ValidationException
+     * @param Request $request
+     * @param Response $response
+     * @param ValidatorService $validatorService
+     * @return Response
      */
-    public function add(Request $request, Response $response): Response
+    public function add(Request $request, Response $response, ValidatorService $validatorService): Response
     {
         $params = $request->getParsedBody();
 
-        if ($this->validatorService->validateCreateOrderData($params)) {
-
-            $client = SimpleClientFactory::createClient('https://superposuda.retailcrm.ru/', 'QlnRWTTWw9lv3kjxy1A8byjUmBQedYqb');
+        if ($validatorService->validateCreateOrderData($params)) {
 
             $request = new OrdersCreateRequest();
             $order = new Order();
@@ -53,10 +36,9 @@ class AddOrderController extends Controller
             $company->brand = $params['brand']; //Бренд
             $offer->externalId = $params['externalId']; //Каталог
             $offer->article = $params['article']; //Артикул
-
-            $item->offer = $offer;
             $item->productName = $params['productName']; //Имя продукта
 
+            $order->site = $params['site']; //Магазин
             $order->orderType = $params['orderType']; //Тип
             $order->status = $params['status']; //Статус
             $order->orderMethod = $params['orderMethod']; //Способ оформления
@@ -66,19 +48,16 @@ class AddOrderController extends Controller
             $order->patronymic = $params['patronymic']; //Отчество
             $order->customerComment = $params['customerComment']; //Коментарий
             $order->customFields = ['prim' => $params['prim']]; //Пользовательское поле примечание (prim)
-            $order->items = $item;
+            $order->items = [$item];
             $order->company = $company;
 
             $request->order = $order;
-            $request->site = $params['site']; //Магазин
 
             try {
-                $data = $client->orders->create($request);
+                $data = $this->client->orders->create($request);
             } catch (ApiExceptionInterface | ClientExceptionInterface $e) {
-                return $this->responseJsonData(['status' => 'Error', 'Message' => $e->getMessage()], $response);
+                return $this->responseJsonData(['status' => 'Error', 'Message' => $e->getMessage(), 'trace' => $e->getTrace()], $response);
             }
-
-            $data = $client->orders->get($data->id, new BySiteRequest(ByIdentifier::EXTERNAL_ID, 'retailcrm'));
 
             return $this->responseJsonData($data, $response);
         }
--- Api/src/Controller/Controller.php
+++ Api/src/Controller/Controller.php
@@ -4,11 +4,27 @@
 namespace App\Controller;
 
 use Psr\Http\Message\ResponseInterface as Response;
+use RetailCrm\Api\Client;
+use RetailCrm\Api\Exception\Client\BuilderException;
+use RetailCrm\Api\Factory\SimpleClientFactory;
 
 class Controller
 {
+    protected Client $client;
+
+    /**
+     * @throws BuilderException
+     */
+    public function __construct()
+    {
+        $this->client = SimpleClientFactory::createClient('https://superposuda.retailcrm.ru/', 'QlnRWTTWw9lv3kjxy1A8byjUmBQedYqb');
+    }
+
     protected function responseJsonData($data, Response $response): Response
     {
+        if (is_object($data)) {
+            $data = (array) $data;
+        }
         if (isset($data['status']) && $data['status'] === 'Error') {
 
             $response = $response->withStatus(400);
